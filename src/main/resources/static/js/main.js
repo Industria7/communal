df405ab23bc683a6d7e1857b25c6fcc05aa623c4
industria7 <night080890>
2020-11-02 21:21:01 +0300

    скрипт активной ссылки меню

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+$('nav ul li a').each(function () {
+        var location = window.location.href;
+        var link = this.href;
+        if(location == link) {
+            $(this).addClass('active');
+        }
+    });
 });
 
