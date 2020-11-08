b35e08443c5a496e4b1eea6ed883a8b370991396
industria7 <night080890>
2020-11-08 23:04:43 +0300

    Исправления

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+$('[data-bs-chart]').each(function(index, elem) {
+		this.chart = new Chart($(elem), $(elem).data('bs-chart'));
+	});
 //$('nav ul li a').each(function () {
 //        var location = window.location.href;
 //        var link = this.href;
